50c7ff728faafc65af0f784eab4f2f1a23398924
Dmitry Kurganov <tylersonorus@gmail.com>
2020-11-02 12:48:38 +0300

    staff block example is added

--- personal/usersStaffPOST.node.ts
+++ personal/usersStaffPOST.node.ts
@@ -10,36 +10,38 @@ interface ResponseData {
 	error?: string; //возвращается в случае ошибки
 }
 
-//Данные body для отправки запроса
-let bodyParams = JSON.stringify({
+/******Пример увольнения сотрудников *********/
+
+//Данные body для отправки запроса на увольнение
+let bodyParamsForDismiss = JSON.stringify({
 	is_active: false,
-    dismissed_date: "2020-09-21" //дата увольнения
+    dismissed_date: "2020-11-21" //дата увольнения
 });
 
-//id сотрудника(ов), которого(ых) редактируем
-let ids = ['140','139'];
+//id сотрудника(ов), которого(ых) редактируем (увольняем)
+let idsForDismiss = ['140','139'];
 
 // Формируем строку параметров
-let queryString = querystring.stringify({
+let queryStringForDismiss = querystring.stringify({
 	token: 'master', //авторизационный токен
-	ids
+	ids:idsForDismiss
 })
 
 //параметры http(s) запроса
-const options = {
+const optionsForDismiss = {
     hostname: 'localhost',
     port: 80,
-    path: `/api/users/staff?${queryString}`,
+    path: `/api/users/staff?${queryStringForDismiss}`,
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(bodyParams),
+        'Content-Length': Buffer.byteLength(bodyParamsForDismiss),
     },
     
 };
 
 //запрос к серверу
-const req = http.request(options, (response) => {
+const reqForDismiss = http.request(optionsForDismiss, (response) => {
 	let data = '';
 	//получаем данные от сервера
 	response.on('data', (chunk) => {
@@ -51,7 +53,7 @@ const req = http.request(options, (response) => {
 		let responseData = JSON.parse(data) as ResponseData;
 		//если сервер вернул код ответа 200, то обрабатываем успешный ответ
 		if (response.statusCode === 200) {
-			console.log(`Сотрудник(и) с id=${ids} успешно уволен(ы):`)
+			console.log(`Сотрудник(и) с id=${idsForDismiss} успешно уволен(ы):`)
 		}
 		//если возникла ошибка на стороне сервера, то выбрасываем ошибку с ее описанием (описание ошибки возвращается серером)
 		else {
@@ -61,12 +63,74 @@ const req = http.request(options, (response) => {
 });
 
 //отправляем тело запроса
-req.write(bodyParams);
+reqForDismiss.write(bodyParamsForDismiss);
 
 //обработка ошибок, возникших при выполнении запроса
-req.on('error', (error) => {
+reqForDismiss.on('error', (error) => {
 	console.error(error.message);
 });
 
 //завершаем запрос
-req.end();
+reqForDismiss.end();
+
+/******Пример блокировки сотрудника *********/
+
+//Данные body для отправки запроса на блокировку
+let bodyParamsForBlock = JSON.stringify({
+	is_block: true,
+});
+
+//id сотрудника(ов), которого(ых) блокируем
+let idsForBlock = ['140','139'];
+
+// Формируем строку параметров
+let queryStringForBlock = querystring.stringify({
+	token: 'master', //авторизационный токен
+	ids:idsForBlock
+})
+
+//параметры http(s) запроса
+const options = {
+    hostname: 'localhost',
+    port: 80,
+    path: `/api/users/staff?${queryStringForBlock}`,
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(bodyParamsForBlock),
+    },
+    
+};
+
+//запрос к серверу
+const reqForBlock = http.request(options, (response) => {
+	let data = '';
+	//получаем данные от сервера
+	response.on('data', (chunk) => {
+		data += chunk;
+	});
+	//обработка полученных данных
+	response.on('end', () => {
+        //декодируем данные в json
+		let responseData = JSON.parse(data) as ResponseData;
+		//если сервер вернул код ответа 200, то обрабатываем успешный ответ
+		if (response.statusCode === 200) {
+			console.log(`Сотрудник(и) с id=${idsForBlock} успешно заблокирован(ы):`)
+		}
+		//если возникла ошибка на стороне сервера, то выбрасываем ошибку с ее описанием (описание ошибки возвращается серером)
+		else {
+			throw new Error(`При выполнении запроса возникла ошибка: ${responseData.error}`);
+		}
+	});
+});
+
+//отправляем тело запроса
+reqForBlock.write(bodyParamsForBlock);
+
+//обработка ошибок, возникших при выполнении запроса
+reqForBlock.on('error', (error) => {
+	console.error(error.message);
+});
+
+//завершаем запрос
+reqForBlock.end();
